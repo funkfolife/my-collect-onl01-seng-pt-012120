42f063cc882d8ff38b5929326b3d0eb9fb51d87f
jonathan e godinez <jongodinezbass@yahoo.com>
2020-02-03 01:30:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,19 @@
 def my_collect(empty_array)
   counter =0 
-  do |x|
+  names = [] 
+  while counter < empty_array
+  names << yield(empty_array [counter])       
          counter += 1
        end
+       names
 end
 
+#def hello(array)
+ # i = 0
+ #collection = []
+  #while i < array.length
+  #collection << yield(array[i])
+   # i += 1
+# end
+ #collection 
+#end
